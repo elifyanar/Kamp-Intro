e6fa83e61bbafe83fdddc67e1b2f72a154960b51
elifyanar <83651273+elifyanar@users.noreply.github.com>
2021-05-22 13:26:02 +0300

    ClassMetotDemo

---
+++ ClassMetotDemo/ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void addCustomer(Customer customer)
+        {
+            Console.WriteLine(customer.Name + " is saved.");
+        }
+        public void displayCustomer(Customer customer)
+        {
+            Console.WriteLine(customer.Id + " " + customer.Name + " " + customer.Surname);
+        }
+        public void deleteCustomer(Customer customer)
+        {
+            Console.WriteLine(customer.Name + " contact deleted");
+        }
+    }
+}
---
+++ ClassMetotDemo/ClassMetotDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            customer.Id = 1;
+            customer.Name = "Elif";
+            customer.Surname = "Yanar";
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.addCustomer(customer);
+            customerManager.displayCustomer(customer);
+            customerManager.deleteCustomer(customer);
+
+        }
+    }
+}
