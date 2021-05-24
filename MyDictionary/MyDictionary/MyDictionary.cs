93928f12cf7712d29ff36e11d83cb3f6ea91a87e
elifyanar <83651273+elifyanar@users.noreply.github.com>
2021-05-24 14:47:33 +0300

    Dictionary

---
+++ MyDictionary/MyDictionary/MyDictionary.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<K, V>
+    {
+        K[] key;
+        V[] value;
+        public MyDictionary()
+        {
+            key = new K[0];
+            value = new V[0];
+        }
+        public void add(K keys, V values)
+        {
+            K[] temporaryKey = key;
+            V[] temporaryValue = value;
+            key = new K[key.Length + 1];
+            value = new V[value.Length + 1];
+            for (int i = 0; i < temporaryKey.Length; i++)
+            {
+                key[i] = temporaryKey[i];
+            }
+            key[key.Length - 1] = keys;
+            for (int i = 0; i < temporaryValue.Length; i++)
+            {
+                value[i] = temporaryValue[i];
+            }
+            value[value.Length - 1] = values;
+        }
+        public V[] Values
+        {
+            get { return value; }
+        }
+        public K[] Keys
+        {
+            get { return key; }
+        }
+    }
+}
+
---
+++ MyDictionary/MyDictionary/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.add(1, "Elif");
+            myDictionary.add(2, "Ayşe");
+           foreach(var isim in myDictionary.Values)
+            {
+                Console.WriteLine(isim);
+            }
+            foreach (var number in myDictionary.Keys)
+            {
+                Console.WriteLine(number);
+            }
+
+
+        }
+    }
+}
